8d6378a47c7d11161d68cf4d2016b33cd8943375
Kirill Konovalov <auddax@gmail.com>
2022-07-26 12:09:06 +0300

    build: add initial config for webpack, eslint and typescript (Tue, Jul 26, 2022 12:09:06 PM)

---
+++ async-race/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "ignorePatterns": ['*config.js'],
+    "rules": {
+        "@typescript-eslint/no-explicit-any": 2
+    }
+}
